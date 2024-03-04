49a5980de5f99bff4782980322897a7e9588f077
BNDPraveen <praveenbabi124@gmail.com>
2024-03-04 20:38:35 +0530

    modified footer

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+//esmodule fix
+const __filname = filenameURLToPath(import.meta.url);
+const __dirname = path.dirname(__filname);
 
 //rest object
 const app = express();
